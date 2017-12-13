01a29cc7a12699398d3807648f1ac5efb5caf1f7
Henry Tran <htran@truecar.com>
2017-12-13 01:50:35 -0800

    Use mongoose

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+var mongoose = require("mongoose");
 
 var usersRoutes = require("./routes/users");
 var dogsRoutes = require("./routes/dogs");
+
+mongoose.connect("mongodb://localhost/basic-react-express-database");
 
 var app = express();
 app.use(bodyParser.json());
@@ -18,6 +21,19 @@ app.use("/api", dogsRoutes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+var User = require("./models/User");
+const riley = new User({
+  name: "Riley",
+  username: "rilddey",
+  password: "hello123"
+});
+
+riley.save(function(err) {
+  if (err) throw err;
+
+  console.log("User saved successfully!");
 });
 
 module.exports = app;
