a93595cbdfb6b477403f306fbf9e5b6fd8174624
Henry Tran <htran@truecar.com>
2017-12-13 01:00:33 -0800

    Add body parser and cookie parser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var usersRoutes = require("./routes/users");
 var dogsRoutes = require("./routes/dogs");
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
