088f11e3841eb44f09eae877f834f40ec3dc4692
Henry Tran <htran@truecar.com>
2018-01-08 23:00:58 -0800

    Make nav bar better looking

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { css, StyleSheet } from "aphrodite";
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/logout">Logout</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/secret">Secret</Link>
-        <Link to="/posts/new">Create a New Post</Link>
+      <div className={css(styles.container)}>
+        <Link className={css(styles.link)} to="/">
+          Home
+        </Link>
+        <Link className={css(styles.link)} to="/users">
+          Users
+        </Link>
+        <Link className={css(styles.link)} to="/login">
+          Login
+        </Link>
+        <Link className={css(styles.link)} to="/logout">
+          Logout
+        </Link>
+        <Link className={css(styles.link)} to="/register">
+          Register
+        </Link>
+        <Link className={css(styles.link)} to="/secret">
+          Secret
+        </Link>
+        <Link className={css(styles.link)} to="/posts/new">
+          Create a New Post
+        </Link>
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1E1E1E",
+    color: "white",
+    display: "flex",
+    justifyContent: "space-around"
+  },
+  link: {
+    color: "white",
+    fontSize: 14,
+    padding: "5px 10px",
+    textAlign: "center",
+    flex: 1,
+    ":hover": {
+      backgroundColor: "#101010",
+      textDecoration: "none"
+    }
+  }
+});
